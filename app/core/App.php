73bb116daf602b880a685c2c5a9d6704b167ca49
Jotosman <if.eighteen@gmail.com>
2019-12-20 22:48:46 +0700

    routing -2

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,18 @@
 class App {
     public function __construct()
     {
-        echo 'OK!';
+        //var_dump($_GET); ini memasuki variable dr url 
+	$url = $this->parseURL();
+	var_dump($url); //ini  menampilkan variable url dgn lbh sederhana
+    }
+
+    public function parseURL()
+    {
+	if( isset($_GET['url']) ) {
+	    $url = rtrim($_GET['url'], '/');
+	    $url = filter_var($url, FILTER_SANITIZE_URL);
+	    $url = explode('/', $url);
+	    return $url;
+	}
     }
 }
