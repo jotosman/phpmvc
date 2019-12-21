3a00b5bf1e4879add560a19374bdd73976838f2b
Jotosman <if.eighteen@gmail.com>
2019-12-21 20:13:29 +0700

    Include Controller -3

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+    public function index($nama = 'Ibrahim', $job = 'Programmer')
+    {
+	echo "Assalammualaikum, nama saya $nama, saya adalah seorang $job";
+    }
+    public function page()
+    {
+	echo 'About/page';
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,11 +1,41 @@
 <?php
 
 class App {
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
         //var_dump($_GET); ini memasuki variable dr url 
 	$url = $this->parseURL();
-	var_dump($url); //ini  menampilkan variable url dgn lbh sederhana
+	//var_dump($url); //ini  menampilkan variable url dgn lbh sederhana
+
+	// controller
+	if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
+	    $this->controller = $url[0];
+	    unset($url[0]);
+	}
+
+	require_once '../app/controllers/' . $this->controller . '.php';
+	$this->controller = new $this->controller;
+
+	// method
+	if( isset($url[1]) ) {
+	    if( method_exists($this->controller, $url[1]) ) {
+		$this->method = $url[1];
+		unset($url[1]);
+            }
+	}
+
+	// params
+	if( !empty($url) ) {
+	    $this->params = array_values($url);
+        } 	    
+        
+	// jalankan controller & method, serta kirimkan params jika ada
+	call_user_func_array([$this->controller, $this->method], $this->params);
+
     }
 
     public function parseURL()
