12efbfdd8e72820eabde3e23bf7c2f318379b5b3
Jotosman <if.eighteen@gmail.com>
2019-12-26 19:46:07 +0700

    add header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Required meta tag -->
     <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-width, intial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootsrap CSS -->
+      
+
     <title>Halaman <?= $data['judul']?></title>
 </head>
 <body>
