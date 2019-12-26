a982a28cf5d52cabb949a5be2421f378a905b14f
Jotosman <if.eighteen@gmail.com>
2019-12-26 17:40:44 +0700

    add view -4

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,21 @@
 <?php
 
-class About {
-    public function index($nama = 'Ibrahim', $job = 'Programmer')
+class About extends Controller {
+    public function index($nama = 'Ibrahim', $pekerjaan = 'Programmer',  $umur = '25')
     {
-	echo "Assalammualaikum, nama saya $nama, saya adalah seorang $job";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-	echo 'About/page';
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Assalammualaikum, nama saya <?= $data['nama']?>, umur saya <?= $data['umur']?> tahun, saya adalah seorang <?= $data['pekerjaan']?>.</p>
+</body>
+</html>
