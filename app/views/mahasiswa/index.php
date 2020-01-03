8108db6fffb2048aca00f493917635923ec41677
Jotosman <if.eighteen@gmail.com>
2020-01-03 09:00:55 +0700

    add insert data -8

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,10 @@
-    <div class="container mt-4">
+    <div class="container mt-3">
         <div class="row">
             <div class="col-6">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                    Tambah Data Mahasiswa
+                </button>
+                <br></br>
                 <h3>Daftar Mahasiswa</h3>
                 <ul class="list-group">
                     <?php foreach( $data['mhs'] as $mhs) : ?>
@@ -10,6 +14,50 @@
                     </li>
                     <?php endforeach; ?>
                 </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap">
+                    </div>
+                    <div class="form-group">
+                        <label for="nrp">nrp</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp" placeholder="nrp Mahasiswa">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="nama@contoh.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Sistem Informasi">Sistem Informasi</option>
+                            <option value="Statistika">Statistika</option>
+                            <option value="Desain Visual">Desain Visual</option>
+                            <option value="Psikologi">Psikologi</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </div>
+            </form>
             </div>
         </div>
     </div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,5 @@
-    <script src="<?= BASEURL; ?>/js/jquery-3.4.1.min.js">
-    <script src="<?= BASEURL; ?>/js/popper.min.js">
-    <script src="<?= BASEURL; ?>/js/bootstrap.min.js">
+    <script src="<?= BASEURL; ?>/js/jquery-3.4.1.min.js"></script>
+    <script src="<?= BASEURL; ?>/js/popper.min.js"></script>
+    <script src="<?= BASEURL; ?>/js/bootstrap.min.js"></script>
 </body>
 </html>
